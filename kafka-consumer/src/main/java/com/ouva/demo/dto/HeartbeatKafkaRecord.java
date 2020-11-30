ce5c68396f37495903cf1f25376999d557da4da8
kagan.akkaya <kagan.akkaya@obss.com.tr>
2020-11-30 15:41:40 +0300

    refactor: add build pattern and validation annotations to dto classes

--- kafka-consumer/src/main/java/com/ouva/demo/dto/HeartbeatKafkaRecord.java
+++ kafka-consumer/src/main/java/com/ouva/demo/dto/HeartbeatKafkaRecord.java
@@ -2,6 +2,10 @@ package com.ouva.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalTime;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -11,15 +15,20 @@ import lombok.ToString;
  */
 @Data
 @ToString
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class HeartbeatKafkaRecord {
 
+  @NotEmpty
   @JsonProperty("room_id")
   private String roomId;
 
+  @NotNull
   @JsonProperty("Red_Signal")
   private Integer redSignal;
 
+  @NotNull
   @JsonProperty("Time")
   private LocalTime time;
 
--- kafka-consumer/src/main/java/com/ouva/demo/model/Heartbeat.java
+++ kafka-consumer/src/main/java/com/ouva/demo/model/Heartbeat.java
@@ -2,13 +2,17 @@ package com.ouva.demo.model;
 
 import java.io.Serializable;
 import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "patient-heartbeats")
 public class Heartbeat implements Serializable {
 
